b6b109a7fa698cfce7dd25839e97984f0ce1180b
danielceballos1 <danielceballos1@gmail.com>
2024-01-27 19:10:30 -0800

    add quiz service

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class QuizService {
+  private currentPageSubject = new BehaviorSubject<number>(1);
+  currentPage$ = this.currentPageSubject;
+
+  constructor(private http: HttpClient) {}
+
+  loadQuestions() {
+    return this.http.get('assets/questions.json');
+  }
+
+  navigateToNextPage() {
+    const nextPage = this.currentPageSubject.value + 1;
+    this.currentPageSubject.next(nextPage);
+  }
+
+  navigateToPreviousPage() {
+    const previousPage = this.currentPageSubject.value - 1;
+    this.currentPageSubject.next(previousPage);
+  }
+}
