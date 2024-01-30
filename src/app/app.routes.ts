ce466d53053c172a07fa80fa851fdc8d517a6944
danielceballos1 <danielceballos1@gmail.com>
2024-01-30 09:53:58 -0800

    add css clean up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { QuizComponent } from './components/quiz/quiz.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'quiz', pathMatch: 'full' },
-    { path: 'quiz', component: QuizComponent },
+ 
+    { path: '', component: QuizComponent }
 ];
